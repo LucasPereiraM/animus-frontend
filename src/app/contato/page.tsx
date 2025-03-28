f7f447a71dce11be9b03e435f21237c7082a32e6
Maria Eduarda <mariaeduardaft2702@mail.com>
2025-03-28 12:13:52 -0300

    🖥️ wip: fixing cvv api and buttons

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -3,6 +3,12 @@ import { useState } from "react";
 import SelectStateCity from "@/components/selectStateCity";
 import ActionButton from "@/components/button";
 import Table from "@/components/table";
+import Link from 'next/link';
+
+type SwitchButtonProps = {
+    name: string,
+    navLink: string
+}
 
 export default function Contato() {
     const [estadoSelecionado, setEstadoSelecionado] = useState<string>("");
@@ -11,20 +17,19 @@ export default function Contato() {
     return (
         <div className="flex flex-row gap-24 justify-center">
             <div className="flex flex-col mt-32 mb-10 gap-5">
-                <ActionButton image="/icons/chat.svg" name="Chat" />
-                <ActionButton image="/icons/phone.svg" name="Ligar 188" />
-                <ActionButton image="/icons/email.svg" name="Email" />
-                <ActionButton image="/icons/point.svg" name="Endereço" />
-                <ActionButton image="/icons/map.svg" name="Exibir Mapa" />
+                <ActionButton image="/icons/chat.svg" name="Chat" alt=""/>
+                <ActionButton image="/icons/phone.svg" name="Ligar 188" alt="" />
             </div>
             <div className="mt-32">
+                <div className="flex flex-row gap-2 ml-4">
+                </div>
                 <SelectStateCity
                     estadoSelecionado={estadoSelecionado}
                     setEstadoSelecionado={setEstadoSelecionado}
                     cidadeSelecionada={cidadeSelecionada}
                     setCidadeSelecionada={setCidadeSelecionada}
                 />
-                <div>
+                <div className="mb-20">
                     <Table estadoSelecionado={estadoSelecionado} cidadeSelecionada={cidadeSelecionada} />
                 </div>
             </div>
